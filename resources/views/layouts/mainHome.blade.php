feffe649c313872ba13272949f9bbd75ca9c0a3d
ilkin <i.abdullaev@nomera.com>
2023-02-12 03:08:49 +0300

    -> Replace asset version

--- resources/views/layouts/mainHome.blade.php
+++ resources/views/layouts/mainHome.blade.php
@@ -48,7 +48,7 @@
   <link href="{{asset('storage/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
 
   <!-- Template Main CSS File -->
-  <link href="{{asset('storage/assets/css/main.css')}}?v=6" rel="stylesheet">
+  <link href="{{asset('storage/assets/css/main.css')}}?v=7" rel="stylesheet">
 </head>
 
 <body>
